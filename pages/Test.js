5a295e6d8108d30caa3cb4ac6a90e257911e08a5
moorels <sales@micropls.com>
2021-09-24 19:42:59 +1000

    ersdr

--- pages/Test.js
+++ pages/Test.js
@@ -9,6 +9,10 @@ const [v,setV] = useState(0)
 const [v1,setV1] = useState(1)
 var [ax,setAx] = useState("   MicroPls Computer Repair Services Melbourne                                                      ")
 const [t1,setT1] = useState(1)
+const [t2,setT2] = useState(1)
+const [t3,setT3] = useState(1)
+const [t4,setT4] = useState(1)
+const [t5,setT5] = useState(1)
 var ax2 = 'Move To Next'
 
 ax.split('')
@@ -33,12 +37,37 @@ useEffect(()=> {
            if(name.length == 1){
             setV(0)
             setV1(1)
-          setAx("  For all your Computer repair needs                                                      ")
+          setAx(" Onsite Laptop and Computer Repairs for Home and Small Business.                                                      ")
             console.log('next')
             setT1(2)
             if(t1 == 2) {
                 setAx("   MicroPls Computer Repair Services Melbourne                                                      ")
-                setT1(1)
+                setT2(2)
+                if(t2 == 2) {
+                    setAx("   Onsite PC, Laptop and Computer Repair                                                      ")
+                    
+                    setT3(2)
+                    if(t3 == 2) {
+                        setAx("   Computer Networking and Installation                                                      ")
+                        setT4(2)
+                        if(t4 == 2) {
+                            setAx("   Data Backup Services                                                      ")
+                            setT5(2)
+                            if(t5 == 2) {
+                            
+                                setAx("   Virus and Malware Removal                                                      ")
+                                setT1(1)
+                                setT2(1)
+                                setT3(1)
+                                setT4(1)
+                                setT5(1)
+                                
+                            }
+                            
+                        }
+                    }
+                }
+                
             }
            }
       },20)
